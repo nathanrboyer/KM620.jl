8ab6bdbefa31b1d1a05c557bef1e074194db444d
nathanrboyer <nathanrobertboyer@gmail.com>
2023-03-01 13:56:33 -0500

    Update dependencies

--- test/latexify_tables.jl
+++ test/latexify_tables.jl
@@ -7,5 +7,6 @@ using DataFrames, Latexify, LaTeXStrings, KM620
 latexify(select(KM620.coefficients_table_for_printing, 1), latex=false)
 
 #'
-latexify(select(KM620.coefficients_table_for_printing, 2:7))
+latexify(select(KM620.coefficients_table_for_printing, Not(1)))
+
 #' *NOTE: Ferritic steel includes carbon, low alloy, and alloy steels, and ferritic, martensitic, and iron-based age-hardening stainless steels.*
