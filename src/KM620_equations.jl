b7bfcb1821f9abc132740653f6c60fa99cd3cc09
nathanrboyer <nathanrobertboyer@gmail.com>
2024-02-14 15:56:42 -0500

    Add `plasticity` function to KM620

--- src/KM620_equations.jl
+++ src/KM620_equations.jl
@@ -30,7 +30,7 @@ using DataFrames, Latexify, LaTeXStrings
 @latexrun ϵ_2(σ_t, A_2, m_2) = (σ_t / A_2)^(1 / m_2)
 
 #' # KM-620.9
-@latexrun A_2(σ_uts, m_2) = (σ_uts * exp(m_2)) / (m_2 ^ m_2)
+@latexrun A_2(σ_uts, m_2) = (σ_uts * exp(m_2)) / (m_2^m_2)
 
 #' # KM-620.10
 @latexrun H(σ_t, σ_ys, σ_uts, K) = 2 * (σ_t - (σ_ys + K * (σ_uts - σ_ys))) / (K * (σ_uts - σ_ys))
@@ -46,3 +46,43 @@ using DataFrames, Latexify, LaTeXStrings
 
 #' # KM-620.14
 @latexrun σ_utst(σ_uts, m_2) = σ_uts * exp(m_2)
+
+#'
+#+ eval=false
+"""
+    plasticity(u, p)
+
+A function in the form required to construct NonlinearSolve.jl's `IntervalNonlinearProblem`.
+It will be used to find the value of `σ_t` which yields `(γ_1 + γ_2) == ϵ_p`.
+This is the point defining the proportional limit and the start of plasticity `(ϵ_p, σ_p)`.
+
+The return value is the difference between the calculated plastic strain `γ_total`
+and the proportional limit `ϵ_p`.
+The root (zero-crossing) of this function will be solved for in the ASME_Materials.jl package
+to determine the start of the plastic stress-strain curve as defined by KM-620.
+Note that the proportional limit `σ_p` determined with this method
+is usually significantly lower than the material yield stress `σ_ys`
+and is a more accurate starting point for the plastic stress-strain curve.
+
+# Arguments
+- `u`: some value of `σ_t`
+- `p`: material parameters; must contain the following fields (which do not depend on `σ_t`):
+    - `σ_ys`
+    - `σ_uts`
+    - `K`
+    - `m_1`
+    - `m_2`
+    - `A_1`
+    - `A_2`
+    - `ϵ_p`
+"""
+function plasticity(u, p)
+    σ_t = u                                          # Solution Variable
+    (; σ_ys, σ_uts, K, m_1, m_2, A_1, A_2, ϵ_p) = p  # Parameters
+    H_value   = H(σ_t, σ_ys, σ_uts, K)               # KM-620.10
+    ϵ_1_value = ϵ_1(σ_t, A_1, m_1)                   # KM-620.5
+    ϵ_2_value = ϵ_2(σ_t, A_2, m_2)                   # KM-620.8
+    γ_1_value = γ_1(ϵ_1_value, H_value)              # KM-620.3
+    γ_2_value = γ_2(ϵ_2_value, H_value)              # KM-620.4
+    return γ_1_value + γ_2_value - ϵ_p  # (<= 0)     # KM-620.2
+end
